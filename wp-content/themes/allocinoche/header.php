c2d449c385c79e13f8facbabb068a9d5419fd067
Jérémy Pichard <jp.jeremypichard@gmail.com>
2017-10-13 10:58:01 +0200

    Ajout Thumbnails Support

--- wp-content/themes/allocinoche/header.php
+++ wp-content/themes/allocinoche/header.php
@@ -15,5 +15,16 @@
     </head>
     <body <?php body_class(); ?>>
         <header id="header">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12">
         <!-- Tout le contenu de l entête de mon site -->
+        <img src="<?= IMAGES_URL;?>/logo.png" class="logo" />
+        <div class='site_information'>
+          <h2><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo ( 'title' ); ?>"><?php bloginfo( 'title' ); ?> </a> </h2>
+          <span><?php bloginfo ('description'); ?></span>
+        </div>
+        </div>
+      </div>
+    </div>
         </header>
--- wp-content/themes/allocinoche/inc/style_and_scripts.php
+++ wp-content/themes/allocinoche/inc/style_and_scripts.php
@@ -5,28 +5,28 @@ add_action( 'wp_enqueue_scripts', 'ajout_scripts');
 function ajout_scripts() {
 
 // enregistrement d'un nouveau script {Bootstrap.js}
-wp_register_script('bootstrap_script', JS_URL. '/assets/scripts/bootstrap.min.js', array('jquery'),'1.1', true);
+wp_register_script('bootstrap_script', JS_URL. '/bootstrap.min.js', array('jquery'),'1.1', true);
 // appel du script dans la page
 wp_enqueue_script('bootstrap_script');
 
 // enregistrement d'un nouveau script {Main.js}
-wp_register_script('main_js', JS_URL.'/assets/scripts/main.js', array('jquery'),'1.1', true);
+wp_register_script('main_js', JS_URL.'/main.js', array('jquery'),'1.1', true);
 // appel du script dans la page
 wp_enqueue_script('main_js');
 
 
 // enregistrement d'un nouveau style {Bootstrap.css}
-wp_register_style( 'bootstrap_style', CSS_URL. '/assets/styles/bootstrap.min.css' );
+wp_register_style( 'bootstrap_style', CSS_URL.'/bootstrap.min.css' );
 // appel du style dans la page
 wp_enqueue_style( 'bootstrap_style' );
 
 // enregistrement d'un nouveau style {Google font}
-wp_register_style( 'google_font', CSS_URL. 'https://fonts.googleapis.com/css?family=Roboto:400,700|Roboto+Slab' );
+wp_register_style( 'google_font', CSS_URL.'https://fonts.googleapis.com/css?family=Roboto:400,700|Roboto+Slab' );
 // appel du style dans la page
 wp_enqueue_style( 'google_font' );
 
 // enregistrement d'un nouveau style {Main.css}
-wp_register_style( 'main_style', CSS_URL. '/assets/styles/main.css' );
+wp_register_style( 'main_style', CSS_URL. '/main.css' );
 // appel du style dans la page
 wp_enqueue_style( 'main_style' );
 
