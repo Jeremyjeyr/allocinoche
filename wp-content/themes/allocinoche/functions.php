3a15efddd10628ec5a48d595e7cfde494632ef43
Jérémy Pichard <jp.jeremypichard@gmail.com>
2017-10-13 10:00:40 +0200

    ajout des scripts et styles

---
+++ wp-content/themes/allocinoche/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'ajout_scripts');
+
+function ajout_scripts() {
+
+// enregistrement d'un nouveau script {Bootstrap.js}
+wp_register_script('bootstrap_script', get_template_directory_uri() . '/assets/scripts/bootstrap.min.js', array('jquery'),'1.1', true);
+// appel du script dans la page
+wp_enqueue_script('bootstrap_script');
+
+// enregistrement d'un nouveau script {Main.js}
+wp_register_script('main_js', get_template_directory_uri() . '/assets/scripts/main.js', array('jquery'),'1.1', true);
+// appel du script dans la page
+wp_enqueue_script('main_js');
+
+
+// enregistrement d'un nouveau style {Bootstrap.css}
+wp_register_style( 'bootstrap_style', get_template_directory_uri() . '/assets/styles/bootstrap.min.css' );
+// appel du style dans la page
+wp_enqueue_style( 'bootstrap_style' );
+
+// enregistrement d'un nouveau style {Main.css}
+wp_register_style( 'main_style', get_template_directory_uri() . '/assets/styles/main.css' );
+// appel du style dans la page
+wp_enqueue_style( 'main_style' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
+
+?>
