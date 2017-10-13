c9200d340d4139cc4adc1650349dc371b23a609e
Jérémy Pichard <jp.jeremypichard@gmail.com>
2017-10-13 10:19:47 +0200

    partition du fichier functions.php

--- wp-content/themes/allocinoche/functions.php
+++ wp-content/themes/allocinoche/functions.php
@@ -1,37 +1,18 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'ajout_scripts');
+define( 'THEME_PATH' ,          get_template_directory()            );
+//define( 'TEMPLATE_PATH' ,       THEME_PATH .    '/templates'        );
 
-function ajout_scripts() {
-
-// enregistrement d'un nouveau script {Bootstrap.js}
-wp_register_script('bootstrap_script', get_template_directory_uri() . '/assets/scripts/bootstrap.min.js', array('jquery'),'1.1', true);
-// appel du script dans la page
-wp_enqueue_script('bootstrap_script');
-
-// enregistrement d'un nouveau script {Main.js}
-wp_register_script('main_js', get_template_directory_uri() . '/assets/scripts/main.js', array('jquery'),'1.1', true);
-// appel du script dans la page
-wp_enqueue_script('main_js');
+define( 'THEME_URL' ,           get_template_directory_uri()        );
+define( 'CSS_URL' ,             THEME_URL .    '/assets/styles'       );
+define( 'IMAGES_URL' ,          THEME_URL .    '/assets/images'       );
+define( 'JS_URL' ,              THEME_URL .    '/assets/scripts'      );
+//define( 'FAVICONS_URL' ,        THEME_URL .    '/dist/favicon'      );
+//define( 'ADMIN_IMAGES_URL' ,    IMAGES_URL .   '/admin'             );
 
 
-// enregistrement d'un nouveau style {Bootstrap.css}
-wp_register_style( 'bootstrap_style', get_template_directory_uri() . '/assets/styles/bootstrap.min.css' );
-// appel du style dans la page
-wp_enqueue_style( 'bootstrap_style' );
-
-// enregistrement d'un nouveau style {Google font}
-wp_register_style( 'google_font', get_template_directory_uri() . 'https://fonts.googleapis.com/css?family=Roboto:400,700|Slabo+27px' );
-// appel du style dans la page
-wp_enqueue_style( 'google_font' );
-
-// enregistrement d'un nouveau style {Main.css}
-wp_register_style( 'main_style', get_template_directory_uri() . '/assets/styles/main.css' );
-// appel du style dans la page
-wp_enqueue_style( 'main_style' );
-
+foreach ( glob( THEME_PATH . "/inc/*.php" ) as $file ) {
+    include_once $file;
 }
-
-add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
 
 ?>
